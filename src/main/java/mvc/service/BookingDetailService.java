c4a4541ce098e85c43180a836c867727f6e03de1
Ngân Phạm <56183330+nganchaoxin@users.noreply.github.com>
2023-03-29 09:51:28 +0700

    Revert "update booking cart function" (#10)

--- src/main/java/mvc/service/BookingDetailService.java
+++ src/main/java/mvc/service/BookingDetailService.java
@@ -1,8 +1,6 @@
 package mvc.service;
 
-import mvc.entity.BookingCartItemEntity;
 import mvc.entity.BookingDetailEntity;
-import mvc.entity.BookingEntity;
 import mvc.repository.BookingDetailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,15 +22,5 @@ public class BookingDetailService {
     }
 
     public List<BookingDetailEntity> findByBookingId(int id) { return  bookingDetailRepository.findAllByBooking_id(id);
-    }
-
-    public void createNewBookingDetail(BookingCartItemEntity cartItem, BookingEntity newBookingEntity) {
-        BookingDetailEntity bookingDetail = new BookingDetailEntity();
-        bookingDetail.setBookingEntity(newBookingEntity);
-        bookingDetail.setRoomEntity(cartItem.getRoomEntity());
-        bookingDetail.setNumber_of_person(cartItem.getRoomEntity().getCategoryEntity().getMax_occupancy());
-        bookingDetail.setBooking_check_in(cartItem.getCheck_in());
-        bookingDetail.setBooking_check_out(cartItem.getCheck_out());
-        bookingDetailRepository.save(bookingDetail);
     }
 }
