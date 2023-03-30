3ff813b513fdd192cd57791fd8e774a812a92d59
nganchaoxin <kimnganphamsu01@gmail.com>
2023-03-30 22:27:37 +0700

    fix cancel booking

--- src/main/java/mvc/controller/UserController.java
+++ src/main/java/mvc/controller/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 import java.text.SimpleDateFormat;
