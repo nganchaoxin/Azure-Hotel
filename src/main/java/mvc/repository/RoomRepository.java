21862314cb6ba973ca240b7c2dce979056533c30
nganchaoxin <kimnganphamsu01@gmail.com>
2023-04-01 23:52:54 +0700

    Change sql query search

--- src/main/java/mvc/repository/RoomRepository.java
+++ src/main/java/mvc/repository/RoomRepository.java
@@ -16,17 +16,17 @@ public interface RoomRepository extends CrudRepository<RoomEntity, Integer> {
     @Query(value = "select room.* , category.* from room join category on category.id = room.category_id where room.id = ?", nativeQuery = true)
     RoomEntity findRoomById(int roomId);
 
-    @Query(value = "SELECT DISTINCT room.*,category.* from room left join category on category.id = room.category_id \n" +
-            "                  LEFT JOIN booking_detail on room.id = booking_detail.room_id \n" +
-            "                where (category.category_name = :roomType) and (:guests <= category.max_occupancy ) \n" +
-            "                and room.id not in \n" +
-            "                (SELECT room.id FROM room left join booking_detail ON booking_detail.room_id = room.id left join booking on booking.id = booking_detail.booking_id\n" +
-            "                WHERE  \n" +
-            "                    (:checkin BETWEEN booking_detail.booking_check_in AND booking_detail.booking_check_out \n" +
-            "                    OR :checkout BETWEEN booking_detail.booking_check_in AND booking_detail.booking_check_out \n" +
-            "                    OR :checkin < booking_detail.booking_check_out AND booking_detail.booking_check_in < :checkout) \n" +
-            "               AND (booking.booking_status = 'COMPLETED')\n" +
-            "                );", nativeQuery = true)
+    @Query(value = "SELECT DISTINCT room.*,category.* from room left join category on category.id = room.category_id  \n" +
+            "                              LEFT JOIN booking_detail on room.id = booking_detail.room_id  \n" +
+            "                            where (category.category_name = :roomType) and (:guests <= category.max_occupancy )  \n" +
+            "                            and room.id not in  \n" +
+            "                            (SELECT room.id FROM room left join booking_detail ON booking_detail.room_id = room.id left join booking on booking.id = booking_detail.booking_id \n" +
+            "                            WHERE   \n" +
+            "                                (booking_detail.booking_check_in <= :checkin <= booking_detail.booking_check_out  \n" +
+            "                                OR booking_detail.booking_check_in <= :checkout <= booking_detail.booking_check_out  \n" +
+            "                                OR :checkin <= booking_detail.booking_check_out AND booking_detail.booking_check_in <= :checkout)  \n" +
+            "                           AND (booking.booking_status = 'COMPLETED') \n" +
+            "                            );", nativeQuery = true)
     List<RoomEntity> getAvailableRooms(@Param("checkin") Date checkin, @Param("checkout") Date checkout, @Param("roomType") String roomType, @Param("guests") int guests);
 
 
