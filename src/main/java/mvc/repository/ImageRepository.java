b183909619c3cf1cb4724093dcb126383b9a314c
Ngân Phạm <56183330+nganchaoxin@users.noreply.github.com>
2023-04-02 18:07:52 +0700

    Revert "Revert "add carousel img in search page""
    This reverts commit dc3e12d45972a2eb75def212697dd5bdc253935d.

--- src/main/java/mvc/repository/ImageRepository.java
+++ src/main/java/mvc/repository/ImageRepository.java
@@ -1,6 +1,7 @@
 package mvc.repository;
 
 import mvc.entity.ImageEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +12,6 @@ public interface ImageRepository extends CrudRepository<ImageEntity, Long> {
     List<ImageEntity> findAll();
 
     ImageEntity findById(long id);
+    @Query(value="select image.* from image join category on image.category_id = category.id where category.category_name = ?;",nativeQuery = true)
+    List<ImageEntity> findAllImageByCategory(String roomType);
 }
