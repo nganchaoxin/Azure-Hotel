ebd57c8967302003ace1a83c9a1fb7a216992431
tamler98 <ngothanhtam98@gmail.com>
2023-04-02 11:36:34 +0700

    update_user_avatar

--- src/main/java/mvc/entity/AccountEntity.java
+++ src/main/java/mvc/entity/AccountEntity.java
@@ -49,6 +49,10 @@ public class AccountEntity implements Serializable {
     @Enumerated(EnumType.STRING)
     private Gender gender;
 
+    @Column(name = "photo")
+    @Lob
+    private byte[] photo;
+
     @Column(name = "birth_date")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date birth_date;
@@ -211,4 +215,11 @@ public class AccountEntity implements Serializable {
         this.token = token;
     }
 
+    public byte[] getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(byte[] photo) {
+        this.photo = photo;
+    }
 }
