fc1880e91efdfb3451d623494d7201277bfdf577
Ngân Phạm <56183330+nganchaoxin@users.noreply.github.com>
2023-04-03 16:38:39 +0700

    Revert "Revert "Add pageable feature""
    This reverts commit cf0809e0b760d6f042f7228002faeafad408a61c.

--- src/main/java/mvc/service/RoomService.java
+++ src/main/java/mvc/service/RoomService.java
@@ -3,6 +3,8 @@ package mvc.service;
 import mvc.entity.RoomEntity;
 import mvc.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -13,8 +15,8 @@ public class RoomService {
     @Autowired
     RoomRepository roomRepository;
 
-    public List<RoomEntity> getAvailableRooms(Date checkin, Date checkout, String roomType, int guests) {
-        List<RoomEntity> availableRooms = roomRepository.getAvailableRooms(checkin, checkout, roomType, guests);
+    public Page<RoomEntity> getAvailableRooms(Date checkin, Date checkout, String roomType, int guests, Pageable pageRequest) {
+        Page<RoomEntity> availableRooms = roomRepository.getAvailableRooms(checkin, checkout, roomType, guests, pageRequest);
 
         return availableRooms;
     }
